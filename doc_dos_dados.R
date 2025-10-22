5e501cbef760bc8b0bbed68edaf8201d7904dcea
Marcel Ferreira <marcelrodrigues45@yahoo.com.br>
2025-10-22 10:21:28 -0300

    documentacao dos dados para aula

---
+++ doc_dos_dados.R
@@ -0,0 +1,8 @@
+#' Example FASTA dataset
+#'
+#' A small example FASTA file with 30 sequences, included for demonstration
+#' purposes in the package.
+#'
+#' @format A character vector of length 6 (FASTA text lines).
+#' @source Simulated data created by Marcel Ferreira.
+"fake.fasta"
